ee390d2485390c1ade09e3b45d792f516d9de14a
LeoPfy <leo.pfyl@stud.fhgr.ch>
2025-10-06 10:25:31 +0200

    j

---
+++ extract.php
@@ -0,0 +1,4 @@
+function fetchMetheData()
+{
+    $url = "https://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9ehttps://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9e"
+}
