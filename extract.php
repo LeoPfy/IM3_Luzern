adccd92b76c70efeee18ff135d0421c6a09c75be
Lifcz <livio.fretz@stud.fhgr.ch>
2025-10-07 10:01:12 +0200

    Extract File erstellt

--- extract.php
+++ extract.php
@@ -1,4 +1,18 @@
+
+<?php
 function fetchMetheData()
 {
-    $url = "https://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9ehttps://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9e"
+    $url = "https://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9e";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    if ($response === false) {
+        echo "Fehler beim Abrufen der API-Daten: ".curl_error($ch);
+        curl_close($ch);
+        return null;
+    }
+    curl_close($ch);
+    return json_decode($response, true);
 }
+
+return fetchMetheData();
